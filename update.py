aa5013b19b0989c6c06d615d8b1961aa4674bb13
Zephyruson <xepydev@gmail.com>
2026-01-05 16:27:39 +0100

    Create update.py

---
+++ update.py
@@ -0,0 +1,13 @@
+import requests
+
+CHANNEL_URL = "https://www.youtube.com/@USERNAME"
+
+html = requests.get(CHANNEL_URL).text
+
+start = html.find('"avatar":{"thumbnails":[{"url":"') + 35
+end = html.find('"', start)
+avatar_url = html[start:end]
+
+img = requests.get(avatar_url).content
+with open("avatar.png", "wb") as f:
+    f.write(img)
